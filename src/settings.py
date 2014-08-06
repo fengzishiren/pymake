10051b266ef71ef31dc9625c8ea278903e999601
Lunatic Zheng <lunaticzheng@gmail.com>
2014-08-06 22:23:43 +0800

    update input/output

--- src/settings.py
+++ src/settings.py
@@ -4,15 +4,15 @@ Created on 2014年8月5日
 
 @author: lunatic
 '''
-Compiler = {'name': 'gcc',
+COMPILER = {'name': 'gcc',
            'optimization':0,
            'debugging':3,
            'warning': True,
            'other': '-fmessage-length=0'}
 
-Target = {'hello.exe': ('main.cc', 'lexer.cc', 'engine.cc', 'alarm.cc'),
+TARGET = {'hello.exe': ('main.cc', 'lexer.cc', 'engine.cc', 'alarm.cc'),
           'test.exe': ('', '', '', ''),
           'test1.exe': ('', '', '', '')}
 
-Input = 'src'
-Output = 'debug'
+INPUT = '../src'
+OUTPUT = '../debug'
