99d5d27922c61f56334c3bf35c8e22536d386956
fengzishiren <xiaoyaozi106@163.com>
2014-08-07 17:21:19 +0800

    delete get_logger

--- src/pymake.py
+++ src/pymake.py
@@ -41,26 +41,6 @@ ch.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(me
 ch.setLevel(logging.DEBUG)
 logger.addHandler(ch)
 
-def get_logger(cls, name):
-    # create logger with 'spam_application'
-    logger = logging.getLogger(name)
-    logger.setLevel(logging.DEBUG)
-    # create file handler which logs even debug messages
-    fh = logging.FileHandler(name + '.log')
-    fh.setLevel(logging.DEBUG)
-    # create console handler with a higher log level
-    ch = logging.StreamHandler()
-    ch.setLevel(logging.DEBUG)
-    # create formatter and add it to the handlers
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    fh.setFormatter(formatter)
-    ch.setFormatter(formatter)
-    # add the handlers to the logger
-    logger.addHandler(fh)
-    logger.addHandler(ch)
-    
-    return logger
-
 class Config:
     parser = ConfigParser.ConfigParser()
     parser.read('build.cfg')
